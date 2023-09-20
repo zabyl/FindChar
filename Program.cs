2467fd425a2047c2ec55140fb6aff92edab03a6d
zabyl <zabylshag@gmail.com>
2023-09-20 17:16:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Lütfen aralarına virgül koyarak bir kelime ve bir index giriniz: ");
+        string input = Console.ReadLine();
+        string[] inputs = input.Split(",");
+        string word = inputs[0];
+        int index = Convert.ToInt32(inputs[1]);
+        Console.WriteLine(word[index]);
+    }
+}
